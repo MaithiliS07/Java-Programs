e662057edcf07407bad63a8752f7d6c970492418
Maithili <salunkhemaithili07@gmail.com>
2023-05-02 00:33:50 +0530

    First Program

---
+++ arithmetic.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class arithmetic{
+    public static void main(String[] args){
+        int a, b, result;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter value for a and b: ");
+        a = s.nextInt();
+        b = s.nextInt();
+    }
+}
