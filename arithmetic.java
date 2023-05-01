f556e6322e1f2bee0fb90f90eb1d399e05aaf98e
Maithili <salunkhemaithili07@gmail.com>
2023-05-02 00:41:57 +0530

    First Program Completed

--- arithmetic.java
+++ arithmetic.java
@@ -1,10 +1,34 @@
 import java.util.Scanner;
 class arithmetic{
     public static void main(String[] args){
-        int a, b, result;
+        int a, b, result, ch;
         Scanner s = new Scanner(System.in);
         System.out.println("Enter value for a and b: ");
         a = s.nextInt();
         b = s.nextInt();
+
+        System.out.println("\n1. Addition\t\t 2. Subtraction\t\t 3. Multiplication\t\t 4. Division");
+        System.out.println("\nEnter your choice: ");
+        ch = s.nextInt();
+
+        switch(ch){
+            case 1: result = (a + b);
+                    System.out.println("Addition is: " + result);
+            break;
+
+            case 2: result = (a - b);
+                    System.out.println("Subtraction is: " + result);
+            break;
+
+            case 3: result = (a * b);
+                    System.out.println("Multiplication is: " + result);
+            break;
+
+            case 4: result = (a / b);
+                    System.out.println("Division is: " + result);
+            break;
+
+            default: System.out.println("\nInvalid Choice!");
+        }
     }
 }
