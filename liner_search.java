b46598fcd4813c50345584f4bfe1f88c23597f6c
Maithili <salunkhemaithili07@gmail.com>
2023-05-05 21:29:18 +0530

    Some changes

--- liner_search.java
+++ liner_search.java
@@ -2,9 +2,22 @@ import java.util.Scanner;
 class liner_search {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a[100], n, x;
+        int a[10], n, x;
         System.out.println("Enter number of elements: ");
         n = sc.nextInt();
-        
+        System.out.println("\nEnter elements of array: ");
+        for(int i = 0; i < n; i++){
+            a[i] = sc.nextInt();
+        }
+
+        System.out.println("\nEnter element to be searched: ");
+        x = sc.nextInt();
+
+        for(int i = 0; i < n; i++){
+            if(a[i] == x){
+                System.out.println("Element found at index: " + i);
+                break;
+            }
+        }
     }
 }
