11413813f34d9febe61bd5a25cb867859f161d03
Maithili <salunkhemaithili07@gmail.com>
2023-05-05 21:25:38 +0530

    New program

---
+++ liner_search.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class liner_search {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a[100], n, x;
+        System.out.println("Enter number of elements: ");
+        n = sc.nextInt();
+        
+    }
+}
